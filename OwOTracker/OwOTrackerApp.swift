d76874933c4791b9dd1ea92599377bbfff05af56
Ferdinand Martini <ferdinand.martini@web.de>
2021-04-04 15:52:04 +0200

    Initial Commit

---
+++ OwOTracker/OwOTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwOTrackerApp.swift
+//  OwOTracker
+//
+//  Created by Ferdinand Martini on 04.04.21.
+//
+
+import SwiftUI
+
+@main
+struct OwOTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
